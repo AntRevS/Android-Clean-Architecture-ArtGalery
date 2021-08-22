05f41dd799281743baac0a58672e5dc245dd7a13
reviakin-package <80742342+reviakin-package@users.noreply.github.com>
2021-08-22 18:36:52 +0400

    Create TestUtil.kt

---
+++ app/src/main/java/com/reviakin_package/clean_architecture/utils/TestUtil.kt
@@ -0,0 +1,23 @@
+package com.reviakin_package.clean_architecture.utils
+
+import com.reviakin_package.clean_architecture.domain.model.Photo
+
+object TestUtil {
+
+    fun createPhoto(id: Long) = Photo(
+        id = id,
+        title = "",
+        url = "",
+        thumbnailUrl = ""
+    )
+
+    fun makePhotoList(size: Int): MutableList<Photo>{
+        val list = ArrayList<Photo>(size)
+        list.forEach{
+            it.title = "Photo ${list.indexOf(it)}"
+            it.id = (list.indexOf(it) + 1).toLong()
+            list.add(it)
+        }
+        return list
+    }
+}
