ca8426f55b83ed2a410ac3435bccee8c01432176
reviakin-package <80742342+reviakin-package@users.noreply.github.com>
2021-08-22 16:28:21 +0400

    Create AlbumsFragment.kt

---
+++ app/src/main/java/com/reviakin_package/clean_architecture/presentation/album/AlbumsFragment.kt
@@ -0,0 +1,92 @@
+package com.reviakin_package.clean_architecture.presentation.album
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import com.reviakin_package.clean_architecture.R
+import com.reviakin_package.clean_architecture.databinding.FragmentAlbumsBinding
+import com.reviakin_package.clean_architecture.domain.model.Album
+import com.reviakin_package.clean_architecture.presentation.gallery.OnGalleryCallback
+import dagger.hilt.android.AndroidEntryPoint
+import java.lang.ClassCastException
+
+@AndroidEntryPoint
+class AlbumsFragment : Fragment(), OnAlbumsAdapterListener {
+
+    private lateinit var fragmentAlbumsBinding: FragmentAlbumsBinding
+    private var adapter: AlbumsAdapter? = null
+    private var mCallback: OnGalleryCallback? = null
+
+    private val viewModel: AlbumsViewModel by viewModels()
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is OnGalleryCallback){
+            mCallback = context
+        }else throw ClassCastException(context.toString() + "must implement OnGalleryCallback")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        adapter = AlbumsAdapter(this)
+        viewModel.loadAlbums()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        fragmentAlbumsBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_albums, container, false)
+        fragmentAlbumsBinding.albumsViewModel = viewModel
+        fragmentAlbumsBinding.albumsRecyclerView.adapter = adapter
+
+        viewModel.isLoad.observe(viewLifecycleOwner, {
+            it?.let { visibility ->
+                fragmentAlbumsBinding.albumsProgressBar.visibility = if (visibility) View.GONE else View.VISIBLE
+            }
+        })
+
+        viewModel.albumsReceivedLiveData.observe(viewLifecycleOwner, {
+            it?.let {
+                initRecyclerView(it)
+            }
+        })
+
+        return fragmentAlbumsBinding.root
+    }
+
+    override fun showPhotos(album: Album) {
+        mCallback?.navigateToAlbumPage(album)
+    }
+
+    private fun initRecyclerView(albums: List<Album>){
+        adapter?.addData(albums)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        adapter = null
+        mCallback = null
+    }
+
+    companion object{
+
+        val FRAGMENT_NAME = AlbumsFragment::class.java.name
+
+        @JvmStatic
+        fun newInstance() =
+            AlbumsFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+
+}
