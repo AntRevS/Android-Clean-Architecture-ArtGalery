f66a6c60c41fcf66802a098607d725ab595d5267
reviakin-package <80742342+reviakin-package@users.noreply.github.com>
2021-08-22 15:49:03 +0400

    Create OnGalleryCallback.kt

---
+++ app/src/main/java/com/reviakin_package/clean_architecture/presentation/gallery/OnGalleryCallback.kt
@@ -0,0 +1,11 @@
+package com.reviakin_package.clean_architecture.presentation.gallery
+
+import android.widget.ImageView
+import com.reviakin_package.clean_architecture.domain.model.Album
+
+interface OnGalleryCallback {
+    
+    fun navigateToAlbumPage(album: Album)
+
+    fun gotroDetailPageByPhotoId(imageView: ImageView, id: Long)
+}
