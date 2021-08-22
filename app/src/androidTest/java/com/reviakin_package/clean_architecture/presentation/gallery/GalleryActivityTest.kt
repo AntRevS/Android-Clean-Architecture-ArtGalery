0854fef7947084df53efcd14b96ddecc961638a8
reviakin-package <80742342+reviakin-package@users.noreply.github.com>
2021-08-22 18:44:14 +0400

    Create GalleryActivityTest.kt

---
+++ app/src/androidTest/java/com/reviakin_package/clean_architecture/presentation/gallery/GalleryActivityTest.kt
@@ -0,0 +1,24 @@
+package com.reviakin_package.clean_architecture.presentation.gallery
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.reviakin_package.clean_architecture.R
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class GalleryActivityTest {
+
+    @get:Rule
+    var activityRule = ActivityTestRule(GalleryActivity::class.java)
+
+    @Test
+    fun container_isDisplayed(){
+        onView(withId(R.id.gallery_container)).check(matches(isDisplayed()))
+    }
+}
